8f14da5e14280b4904ea905a39181413d40cdd9e
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2015-07-23 22:15:02 +0400

    Add script to open hidden elements

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,11 @@
-
+// To open hidden elements
+$('.js-uncap').click(function() {
+    var group = $(this).data('group');
+    $('.js-hidden[data-group="' + group + '"]').removeClass('js-hidden');
+    $(this).removeClass('clicable-row');
+    $(this).find('.icon-container').remove();
+    $(this).find('.js-view-more').remove();
+});
 
 
 
